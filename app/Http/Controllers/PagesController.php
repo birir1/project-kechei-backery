b2e8044178d10c6da546ef5dc56109ee599594a2
birir1 <sospeterbirir1@gmail.com>
2024-10-23 14:51:44 +0300

    version 1.0.0

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,5 +6,63 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('frontend.index');
+    }
+
+    public function about()
+    {
+        return view('frontend.about');
+    }
+
+    public function products()
+    {
+        return view('frontend.products');
+    }
+
+    public function orderOnline()
+    {
+        return view('frontend.order');
+    }
+
+    public function catering()
+    {
+        return view('frontend.catering');
+    }
+
+    public function recipes()
+    {
+        return view('frontend.recipes');
+    }
+
+    public function events()
+    {
+        return view('frontend.events');
+    }
+
+    public function contact()
+    {
+        return view('frontend.contact');
+    }
+
+    public function blog()
+    {
+        return view('frontend.blog');
+    }
+
+    public function faq()
+    {
+        return view('frontend.faq');
+    }
+
+    public function privacy()
+    {
+        return view('frontend.privacy');
+    }
+
+    public function terms()
+    {
+        return view('frontend.terms');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,25 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\PagesController;
+
+Route::get('/', [PagesController::class, 'index'])->name('index');
+Route::get('/about', [PagesController::class, 'about'])->name('about');
+Route::get('/products', [PagesController::class, 'products'])->name('products');
+Route::get('/order', [PagesController::class, 'orderOnline'])->name('order');
+Route::get('/catering', [PagesController::class, 'catering'])->name('catering');
+Route::get('/recipes', [PagesController::class, 'recipes'])->name('recipes');
+Route::get('/events', [PagesController::class, 'events'])->name('events');
+Route::get('/contact', [PagesController::class, 'contact'])->name('contact');
+Route::get('/blog', [PagesController::class, 'blog'])->name('blog');
+Route::get('/faq', [PagesController::class, 'faq'])->name('faq');
+Route::get('/privacy', [PagesController::class, 'privacy'])->name('privacy');
+Route::get('/terms', [PagesController::class, 'terms'])->name('terms');
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
